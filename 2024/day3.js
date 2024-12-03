1ace7cc07dbaebe86ef000b06f8fa635ff553d98
L'eau Artésienne <24992458+aqua-lzma@users.noreply.github.com>
2024-12-03 05:27:19 +0000

    advent of regex

--- 2024/day3.js
+++ 2024/day3.js
@@ -1,29 +1,37 @@
 import { log, getInput } from '../helpers/aoc.js'
 
-const input = await getInput(0, 0)
-const ex1 = `
-`
+const input = await getInput(2024, 3)
+const ex1 = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`
+const ex2 = `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`
 
 function parseInput (input) {
-  return input.split('\n')
-    .map(i => i)
+  return input
 }
 
 function part1 (input) {
   input = parseInput(input)
+  let sum = 0
+  const regex = /mul\((\d{1,3}),(\d{1,3})\)/g
+  for (const [_, a, b] of input.matchAll(regex)) {
+    sum += parseInt(a) * parseInt(b)
+  }
+  return sum
 }
 
 function part2 (input) {
   input = parseInput(input)
+  let enabled = true
+  let sum = 0
+  const regex = /mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)/g
+  for (const [match, a, b] of input.matchAll(regex)) {
+    if (match === 'do()') { enabled = true; continue }
+    if (match === "don't()") { enabled = false; continue }
+    if (enabled) sum += parseInt(a) * parseInt(b)
+  }
+  return sum
 }
 
-log('Part 1 example', part1, [ex1])
+log('Part 1 example', part1, [ex1], 161)
 log('Part 1 input', part1, [input])
-log('Part 2 example', part2, [ex1])
+log('Part 2 example', part2, [ex2], 48)
 log('Part 2 input', part2, [input])
-
-/*
-const bigboy = await getInput(0, '0-bigboy')
-log('Part 1 bigboy', part1, [bigboy])
-log('Part 2 bigboy', part2, [bigboy])
-*/
