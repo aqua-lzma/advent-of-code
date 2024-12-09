5612b0ce3eb8c8bd05f7e7ee46f51c803c916f0f
aqua-lzma <24992458+aqua-lzma@users.noreply.github.com>
2024-12-09 17:04:14 +0900

    mmmmmm memory yum yum

--- 2024/day9.js
+++ 2024/day9.js
@@ -1,77 +1,64 @@
 import { log, getInput } from '../helpers/aoc.js'
 
 const input = await getInput(2024, 9)
-const ex1 = '12345'
-const ex2 = '2333133121414131402'
+const ex1 = '2333133121414131402'
 
 function parseInput (input) {
-  return input.split('').map(i => parseInt(i))
+  return new Array(Math.ceil(input.length / 2)).fill().map((_, index) => {
+    const file = parseInt(input[index * 2] ?? 0)
+    const free = parseInt(input[(index * 2) + 1] ?? 0)
+    return { id: index, file, free, written: false }
+  })
 }
 
 function part1 (input) {
   input = parseInput(input)
-  const str = []
-  for (let i = 0; i < input.length; i++) {
-    if (i % 2 === 0) {
-      str.push(...(new Array(input[i]).fill(i / 2)))
-    } else {
-      str.push(...(new Array(input[i])))
-    }
-  }
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] == null) {
-      let j = str.pop()
-      while (j == null) j = str.pop()
-      str[i] = j
-    }
-  }
   let out = 0
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] == null) continue
-    out += i * str[i]
+  let index = 0
+  while (input.length > 0) {
+    const { id, file, free } = input.shift()
+    for (let i = 0; i < file; i++) {
+      out += id * index++
+    }
+    for (let i = 0; i < free; i++) {
+      if (input.length === 0) break
+      const last = input.at(-1)
+      out += last.id * index++
+      last.file--
+      if (last.file === 0) input.pop()
+    }
   }
   return out
 }
 
 function part2 (input) {
   input = parseInput(input)
-  input = input.map(i => ({ value: i, written: false }))
-  const str = []
-  for (let i = 0; i < input.length; i++) {
-    const { value, written } = input[i]
-    if (i % 2 === 0) {
-      if (written) str.push(...(new Array(value)))
-      else str.push(...(new Array(value).fill(i / 2)))
-    } else {
-      let remainder = value
-      let reset = true
-      while (reset && remainder > 0) {
-        reset = false
-        for (let j = input.length - 1; j > i; j -= 2) {
-          const { value, written } = input[j]
-          if (written) continue
-          if (value <= remainder) {
-            str.push(...(new Array(value).fill(j / 2)))
-            remainder -= value
-            input[j].written = true
-            reset = true
-            break
-          }
-        }
-      }
-      str.push(...(new Array(remainder)))
-    }
-  }
   let out = 0
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] == null) continue
-    out += i * str[i]
+  let index = 0
+  while (input.length > 0) {
+    let { id, file, free, written } = input.shift()
+    if (!written) {
+      for (let i = 0; i < file; i++) {
+        out += id * index++
+      }
+    } else index += file
+
+    let last = input.findLast(({ file, written }) => file <= free && !written)
+    while (last != null) {
+      for (let i = 0; i < last.file; i++) {
+        out += last.id * index++
+      }
+      free -= last.file
+      last.written = true
+      if (input.at(-1).written) input.pop()
+      last = input.findLast(({ file, written }) => file <= free && !written)
+    }
+    index += free
   }
   return out
 }
 
-log('Part 1 example', part1, [ex1])
-log('Part 1 example', part1, [ex2])
+log('Part 1 example', part1, [ex1], 1928)
 log('Part 1 input', part1, [input])
-log('Part 2 example', part2, [ex2])
+log('Part 2 example', part2, [ex1], 2858)
 log('Part 2 input', part2, [input])
