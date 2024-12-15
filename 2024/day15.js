2f4a8ec7b2e75eb921d2dec430f369b35cde6f4e
aqua-lzma <24992458+aqua-lzma@users.noreply.github.com>
2024-12-15 15:40:05 +0900

    cleaned up code looks cute

--- 2024/day15.js
+++ 2024/day15.js
@@ -42,13 +42,6 @@ const ex3 = `#######
 
 <vv<<^^<<^^`
 
-function parseInput (input) {
-  let [grid, moves] = input.split('\n\n')
-  grid = grid.split('\n').map(line => Array.from(line))
-  moves = Array.from(moves).filter(i => i !== '\n')
-  return { grid, moves }
-}
-
 /* eslint-disable quote-props */
 const dirs = {
   '>': [1, 0],
@@ -58,40 +51,70 @@ const dirs = {
 }
 /* eslint-enable quote-props */
 
-function part1 (input) {
-  const { grid, moves } = parseInput(input)
-  let pos
-  for (let y = 0; y < grid.length; y++) {
-    for (let x = 0; x < grid[0].length; x++) {
+function parseInput (input) {
+  let [grid, moves] = input.split('\n\n')
+  grid = grid.split('\n').map(line => Array.from(line))
+  moves = Array.from(moves).filter(i => i !== '\n')
+  return { grid, moves }
+}
+
+function getPos (grid) {
+  for (let y = 1; y < grid.length - 1; y++) {
+    for (let x = 1; x < grid[0].length - 1; x++) {
       if (grid[y][x] === '@') {
         grid[y][x] = '.'
-        pos = { x, y }
+        return { x, y }
+      }
+    }
+  }
+}
+
+function getBoxes (grid, x, y, dx, dy) {
+  if (grid[y + dy][x + dx] === '#') return
+  if (grid[y + dy][x + dx] === '.') return []
+  const toMove = [[x + dx, y + dy]]
+  if (grid[y + dy][x + dx] === '[') toMove.push([x + dx + 1, y + dy])
+  if (grid[y + dy][x + dx] === ']') toMove.push([x + dx - 1, y + dy])
+  let canMove = false
+  while (!canMove) {
+    canMove = true
+    for (const [x, y] of toMove) {
+      if (toMove.some(([x2, y2]) => x + dx === x2 && y + dy === y2)) continue
+      if (grid[y + dy][x + dx] === '#') return
+      if (grid[y + dy][x + dx] === '.') continue
+      canMove = false
+      if (grid[y + dy][x + dx] === 'O') toMove.push([x + dx, y + dy])
+      if (grid[y + dy][x + dx] === '[') {
+        toMove.push([x + dx, y + dy])
+        if (dy !== 0) toMove.push([x + dx + 1, y + dy])
+      }
+      if (grid[y + dy][x + dx] === ']') {
+        toMove.push([x + dx, y + dy])
+        if (dy !== 0) toMove.push([x + dx - 1, y + dy])
+      }
+    }
+  }
+  return toMove
+}
+
+function part1 (input) {
+  const { grid, moves } = parseInput(input)
+
+  let { x, y } = getPos(grid)
+  for (const dir of moves) {
+    const [dx, dy] = dirs[dir]
+    const boxes = getBoxes(grid, x, y, dx, dy)
+    if (boxes != null) {
+      x += dx
+      y += dy
+      while (boxes.length > 0) {
+        const [x, y] = boxes.pop()
+        grid[y + dy][x + dx] = grid[y][x]
+        grid[y][x] = '.'
       }
     }
   }
 
-  for (const move of moves) {
-    const [dx, dy] = dirs[move]
-    const next = grid[pos.y + dy][pos.x + dx]
-    if (next === '.') {
-      pos.x += dx
-      pos.y += dy
-    } else if (next === 'O') {
-      let found = false
-      let [x2, y2] = [pos.x + dx, pos.y + dy]
-      while (grid[y2][x2] !== '#') {
-        if (grid[y2][x2] === '.') { found = true; break }
-        x2 += dx
-        y2 += dy
-      }
-      if (found) {
-        grid[pos.y + dy][pos.x + dx] = '.'
-        grid[y2][x2] = 'O'
-        pos.x += dx
-        pos.y += dy
-      }
-    }
-  }
   let out = 0
   for (let y = 1; y < grid.length - 1; y++) {
     for (let x = 1; x < grid[0].length - 1; x++) {
@@ -106,83 +129,24 @@ function part2 (input) {
   grid = grid.map(line => {
     line = line.join('')
     line = line.replace(/#/g, '##')
-    line = line.replace(/\./g, '..')
     line = line.replace(/O/g, '[]')
+    line = line.replace(/\./g, '..')
     line = line.replace(/@/g, '@.')
     return Array.from(line)
   })
 
-  let pos
-  for (let y = 0; y < grid.length; y++) {
-    for (let x = 0; x < grid[0].length; x++) {
-      if (grid[y][x] === '@') {
-        grid[y][x] = '.'
-        pos = { x, y }
-      }
-    }
-  }
+  let { x, y } = getPos(grid)
 
-  for (const move of moves) {
-    const [dx, dy] = dirs[move]
-    const next = grid[pos.y + dy][pos.x + dx]
-    if (next === '.') {
-      pos.x += dx
-      pos.y += dy
-    } else if (next === '[' || next === ']') {
-      if (move === '<' || move === '>') {
-        let found = false
-        let [x2, y2] = [pos.x + dx, pos.y + dy]
-        const toMove = []
-        while (grid[y2][x2] !== '#') {
-          if (grid[y2][x2] === '.') { found = true; break }
-          toMove.push([x2, y2])
-          x2 += dx
-          y2 += dy
-        }
-        if (found) {
-          while (toMove.length > 0) {
-            const [x, y] = toMove.pop()
-            grid[y + dy][x + dx] = grid[y][x]
-            grid[y][x] = '.'
-          }
-          pos.x += dx
-          pos.y += dy
-        }
-      } else {
-        const toMove = [[pos.x + dx, pos.y + dy]]
-        if (next === '[') toMove.push([pos.x + dx + 1, pos.y + dy])
-        else toMove.push([pos.x + dx - 1, pos.y + dy])
-        let canMove = false
-        while (!canMove) {
-          canMove = true
-          let wall = false
-          for (const [x, y] of toMove) {
-            if (toMove.some(([x2, y2]) => x + dx === x2 && y + dy === y2)) continue
-            if (grid[y + dy][x + dx] === '#') {
-              canMove = false
-              wall = true
-              break
-            }
-            if (grid[y + dy][x + dx] === '[') {
-              toMove.push([x + dx, y + dy], [x + dx + 1, y + dy])
-              canMove = false
-            }
-            if (grid[y + dy][x + dx] === ']') {
-              toMove.push([x + dx, y + dy], [x + dx - 1, y + dy])
-              canMove = false
-            }
-          }
-          if (wall) break
-        }
-        if (canMove) {
-          while (toMove.length > 0) {
-            const [x, y] = toMove.pop()
-            grid[y + dy][x + dx] = grid[y][x]
-            grid[y][x] = '.'
-          }
-          pos.x += dx
-          pos.y += dy
-        }
+  for (const dir of moves) {
+    const [dx, dy] = dirs[dir]
+    const boxes = getBoxes(grid, x, y, dx, dy)
+    if (boxes != null) {
+      x += dx
+      y += dy
+      while (boxes.length > 0) {
+        const [x, y] = boxes.pop()
+        grid[y + dy][x + dx] = grid[y][x]
+        grid[y][x] = '.'
       }
     }
   }
@@ -196,8 +160,10 @@ function part2 (input) {
   return out
 }
 
-log('Part 1 example 1', part1, [ex1])
-log('Part 1 example 2', part1, [ex2])
+log('Part 1 example 1', part1, [ex1], 2028)
+log('Part 1 example 2', part1, [ex2], 10092)
 log('Part 1 input', part1, [input])
-log('Part 2 example', part2, [ex2])
+log('Part 2 example 1', part2, [ex1], 1751)
+log('Part 2 example 2', part2, [ex2], 9021)
+log('Part 2 example 3', part2, [ex3], 618)
 log('Part 2 input', part2, [input])
